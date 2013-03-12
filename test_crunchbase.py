0344f347056fa382095d6c67a9e1e2bf68c8d9d6
AlexanderPease <alexanderpease@usv.com>
2013-03-12 15:32:56 -0400

    Added functions to list investors in a company and vice versa

---
+++ test_crunchbase.py
@@ -0,0 +1,18 @@
+from crunchbase import CrunchBase
+import doctest
+doctest.testfile('README.md', verbose=False)
+
+cb = CrunchBase('j4rufy6d4zckdka4cwxhdeda')
+#company = cb.getCompanyData('Fundly')
+'''
+print company
+print company['name']
+print company['total_money_raised']
+print company['funding_rounds'][1]['investments']#['financial_org']
+'''
+#investors = cb.listCompanyInvestors('Fundly')
+#print investors
+
+investor  = cb.listInvestorPortfolio('Kapor Capital')
+for i in investor:
+	print i
