5bc0a1ea7914360c6f2b4d93bc0309a1622c5a8a
Patrick Reilly <preilly@php.net>
2013-08-02 15:38:59 -0700

    PEP8 Fixe(s)
      1 setup.py|10| 19: E251 unexpected spaces around keyword / parameter equals¬
      2 setup.py|10| 21: E251 unexpected spaces around keyword / parameter equals¬
      3 setup.py|20| 80: E501 line too long (82 > 79 characters)¬
      4 setup.py|22| 8: E127 continuation line over-indented for visual indent¬
      5 setup.py|24| 1: W391 blank line at end of file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name="python-crunchbase",
       py_modules=["crunchbase"],
       description="Libraries for interacting with the Crunchbase API",
       author="Apurva Mehta <mehta.apurva@gmail.com>",
-      author_email = "mehta.apurva@gmail.com",
+      author_email="mehta.apurva@gmail.com",
       license="WTFPL",
       url="http://github.com/amehta/crunchbase",
       long_description=long_description,
@@ -17,8 +17,8 @@ setup(name="python-crunchbase",
                    "Natural Language :: English",
                    "Operating System :: OS Independent",
                    "Programming Language :: Python",
-                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Topic :: Software Development" +
+                   " :: Libraries :: Python Modules",
                    ],
-       install_requires=["simplejson >= 1.8"]
+      install_requires=["simplejson >= 1.8"]
       )
-
