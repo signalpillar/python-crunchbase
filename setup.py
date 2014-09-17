b88628bfb60828c310be13111c2a6a57b874261b
Brian Anglin <laxnski@gmail.com>
2014-09-17 11:36:21 -0700

    Fixes setup.py for submission

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ from distutils.core import setup
 long_description = open('README.md').read()
 
 setup(name="python-crunchbase",
-      version="2.0.1",
-      py_modules=["crunchbase"],
+      version="1.0.1",
+      packages=["crunchbase"],
       description="Libraries for interacting with the Crunchbase 2.0 API",
       author="Brian Anglin <brianranglin@gmail.com>",
       author_email="brianranglin@gmail.com",
       license="WTFPL",
       url="https://github.com/anglinb/python-crunchbase",
       long_description=long_description,
+      download_url = 'https://github.com/anglinb/python-crunchbase/tarball/1.0.1',
       platforms=["any"],
-      classifiers=["Development Status :: 3 - Alpha",
-                   "Intended Audience :: Developers",
+      classifiers=["Intended Audience :: Developers",
                    "Natural Language :: English",
                    "Operating System :: OS Independent",
                    "Programming Language :: Python",
                    "Topic :: Software Development" +
                    " :: Libraries :: Python Modules",
                    ],
-      install_requires=["simplejson >= 1.8"]
+      keywords = ['crunchbase','2.0','api']
       )
